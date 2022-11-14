95f6beb732a95e950fc64157f166b5b881b6f3c3
Imanbekov007 <osmonimanbekov.ksl@gmail.com>
2022-11-14 15:29:01 +0600

    Initial commit

---
+++ src/Circle.java
@@ -0,0 +1,9 @@
+public class Circle {
+
+    public static void area(int radius){
+        System.out.println(Math.PI*(radius*radius));
+    }
+    public static void circumference(int radius){
+        System.out.println(Math.PI*2*radius);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        Circle circle=new Circle();
+
+        circle.area(scanner.nextInt());
+        circle.circumference(scanner.nextInt());
+
+    }
+}
